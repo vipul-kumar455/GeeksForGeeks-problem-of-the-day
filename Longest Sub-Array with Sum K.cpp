f556b885b4aa8a60f0916afed82ad4b8ad360742
Vipul Kumar <74072895+Cyber321-glit-majer@users.noreply.github.com>
2022-08-18 23:37:11 +0530

    Update Longest Sub-Array with Sum K.cpp
    Given an array containing N integers and an integer K., Your task is to find the length of the longest Sub-Array with the sum of the elements equal to the given value K.
    
     
    
    Example 1:
     
    
    Input :
    A[] = {10, 5, 2, 7, 1, 9}
    K = 15
    Output : 4
    Explanation:
    The sub-array is {5, 2, 7, 1}.
    Example 2:
    
    Input : 
    A[] = {-1, 2, 3}
    K = 6
    Output : 0
    Explanation: 
    There is no such sub-array with sum 6.
    Your Task:
    This is a function problem. The input is already taken care of by the driver code. You only need to complete the function smallestSubsegment() that takes an array (A), sizeOfArray (n),  sum (K)and returns the required length of the longest Sub-Array. The driver code takes care of the printing.
    
    Expected Time Complexity: O(N).
    Expected Auxiliary Space: O(N).
    
     
    
    Constraints:
    1<=N<=105
    -105<=A[i], K<=105

--- Longest Sub-Array with Sum K.cpp
+++ Longest Sub-Array with Sum K.cpp
@@ -1,1 +1,26 @@
 
+class Solution{
+    public:
+    int lenOfLongSubarr(int a[],  int n, int k) 
+    { 
+       unordered_map<int,int>mp;
+       int sum=0;
+       int maxi=0;
+       for(int i=0;i<n;i++)
+       {
+           sum+=a[i];
+           if(sum==k)
+           {
+               maxi=max(maxi,i+1);
+           }
+           if(mp.find(sum-k)!=mp.end())
+           {
+               maxi=max(maxi,i-mp[sum-k]);
+           }
+           if(mp.find(sum)==mp.end())
+           mp[sum]=i;
+       }
+       return maxi;
+    } 
+
+};

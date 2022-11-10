7d8fbe49366f3df6e09c8cdae5bee04bc954956d
Vipul Kumar <74072895+Cyber321-glit-majer@users.noreply.github.com>
2022-11-11 01:31:15 +0530

    Create Primes sum.cpp
    Given a number N. Find if it can be expressed as sum of two prime numbers.
    
    Example 1:
    
    Input: N = 34
    Output: "Yes" 
    Explanation: 34 can be expressed as 
    sum of two prime numbers.
    Example 2:
    
    Input: N = 23
    Output: "No"
    Explanation: 23 cannnot be expressed as
    sum of two prime numbers. 
    
    Your Task:  
    You dont need to read input or print anything. Complete the function isSumOfTwo() which takes N as input parameter and returns "Yes" if can be expressed as sum of two prime numbers. else return "No".
    
    Expected Time Complexity: O(N*sqrt(N))
    Expected Auxiliary Space: O(1)
    
    Constraints:
    1 <= N <= 105

---
+++ Primes sum.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+public:
+bool isPrime(int n)
+{
+    // Corner case
+    if (n <= 1)
+        return false;
+ 
+    // Check from 2 to n-1
+    for (int i = 2; i < n; i++)
+        if (n % i == 0)
+            return false;
+ 
+    return true;
+}
+    string isSumOfTwo(int N){
+        // code here
+        int num=N-2;
+        if(N%2!=0 && !isPrime(num))
+        {
+            
+            
+            return "No";
+            
+        }
+        else
+        return "Yes";
+    }
+   
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.isSumOfTwo(N) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends

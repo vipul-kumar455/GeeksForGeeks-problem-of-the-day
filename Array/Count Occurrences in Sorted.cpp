d2ac85e75ee8a0ae3c51b2d4710106646b80d646
Vipul Kumar <74072895+Cyber321-glit-majer@users.noreply.github.com>
2023-02-10 02:06:55 +0530

    Create Count Occurrences in Sorted.cpp

---
+++ Array/Count Occurrences in Sorted.cpp
@@ -0,0 +1,71 @@
+
+#include <iostream>
+
+using namespace std;
+int solve(int a[],int n,int l,int h,int x)
+{
+    
+    
+    while(l<h)
+    {
+        int mid=(l+h)/2;
+        if(a[mid]>x)
+        h=mid-1;
+        else if(x>a[mid])
+        l=mid+1;
+        else
+        {
+            if(mid==0 || a[mid-1]!=a[mid])
+            return mid;
+            else
+           h=mid-1;
+        }
+        
+    }
+    return  -1;
+    
+}
+
+int solve1(int a[],int n,int l,int h,int x)
+{
+    
+     if(l>h)
+     return -1;
+        int mid=(l+h)/2;
+        if(a[mid]>x)
+        return solve1(a,n,l,mid-1,x);
+        else if(x>a[mid])
+       
+           return solve1(a,n,mid+1,h,x);
+        else
+        {
+            if(mid==n-1 || a[mid]!=a[mid+1])
+            return mid;
+            else
+            
+            return solve1(a,n,mid+1,h,x);
+        }
+        
+    
+    
+}
+int count(int a[],int n,int l,int h,int x)
+{
+    int first=solve(a,n,0,n-1,x);
+    if(first==-1)
+    return 0;
+    else
+    return (solve1(a,n,0,n-1,x)-first+1);
+    
+}
+int main()
+{
+    int a[]={1,2,3,4,4,4,5,6};
+    int n=sizeof(a)/sizeof(a[0]);
+    int x=4;
+   // cout<<solve(a,n,0,n-1,x);
+    cout<<count(a,n,0,n-1,x);
+    
+
+    return 0;
+}

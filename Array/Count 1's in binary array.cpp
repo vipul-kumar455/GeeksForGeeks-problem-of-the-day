46b8c77476123627efeb0e66c1747fd4b8276c81
Vipul Kumar <74072895+Cyber321-glit-majer@users.noreply.github.com>
2023-02-11 16:13:19 +0530

    Create Count 1's in binary array.cpp

---
+++ Array/Count 1's in binary array.cpp
@@ -0,0 +1,13 @@
+ int countOnes(int a[], int n)
+    {
+       
+         int cnt=0;
+         for(int i=0;i<n;i++)
+         {
+             if(a[i]==1)
+             cnt++;
+         }
+         return cnt;
+         
+        
+    }

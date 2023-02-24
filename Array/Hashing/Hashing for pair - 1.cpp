dc9ad99b0ab897e89c7aa7f79781b7927bd2c653
Vipul Kumar <74072895+Cyber321-glit-majer@users.noreply.github.com>
2023-02-24 16:51:14 +0530

    Create Hashing for pair - 1.cpp

---
+++ Array/Hashing/Hashing for pair - 1.cpp
@@ -0,0 +1,11 @@
+int sumExists(int arr[], int n, int sum) {
+     unordered_set<int>mp;
+      for(int i=0;i<n;i++)
+      {
+          if(mp.find(sum-arr[i])!=mp.end())
+          return 1;
+          else
+          mp.insert(arr[i]);
+      }
+      return 0;
+}

7af3eecdcb5559736fce0731e8c200ef5804fe88
Vipul Kumar <74072895+Cyber321-glit-majer@users.noreply.github.com>
2023-03-03 02:27:56 +0530

    Update Leftmost Non-repeating Element.cpp

--- string/Leftmost Non-repeating Element.cpp
+++ string/Leftmost Non-repeating Element.cpp
@@ -74,3 +74,36 @@ int main()
 
     return 0;
 }
+
+
+//most used methiod
+
+ 
+#include <bits/stdc++.h>
+
+using namespace std;
+const int CHAR=256; 
+int sol(string s)
+{
+    int n=s.size();
+     
+     int cnt[CHAR]={0};
+     for(int i=0;i<n;i++)
+     {
+         cnt[s[i]]++;
+     }
+     for(int i=0;i<n;i++)
+     {
+         if(cnt[s[i]]==1)
+         return i;
+     }
+     return -1;
+}
+ 
+int main()
+{
+    string s="geeksfogr";
+    cout<<sol(s);
+
+    return 0;
+}

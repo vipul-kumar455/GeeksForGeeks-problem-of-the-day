0ed7ff3c10a557642b774603907b482057e2895f
Vipul Kumar <74072895+Cyber321-glit-majer@users.noreply.github.com>
2023-03-10 23:52:01 +0530

    Update Stock span problem.cpp

--- Stack/Stock span problem.cpp
+++ Stack/Stock span problem.cpp
@@ -36,3 +36,46 @@ return v1;
  }
 
 //Time complexity:: Theta(n)
+
+//BRUTE FORCE APPROACH
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printSpan(int arr[],int n){
+    for(int i=0;i<n;i++){
+        int span=1;
+        for(int j=i-1;j>=0&&arr[j]<=arr[i];j--)
+        {
+             
+            span++;
+        }
+        cout<<span<<" ";
+        
+    }    
+}
+void sol(int a[],int n)
+{
+   
+    for(int i=0;i<n;i++)
+    { int span=1;
+        for(int j=i-1;j>=0 && a[j]<=a[i];j--)
+        {
+            span++;
+        }
+        cout<<span<<" ";
+    }
+    
+}
+
+int main() 
+{ 
+    int arr[]={18,12,13,14,11,16};
+    int n=6;
+    printSpan(arr,n);
+    cout<<endl;
+    sol(arr,n);
+    return 0; 
+}
+
